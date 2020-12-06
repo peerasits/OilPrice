afaaf9657d81681004404e5e6d292a739c45776c
Peerasit Suntieakchun <peerasit11@gmail.com>
2020-12-06 23:41:28 +0700

    add time

--- app/src/main/java/th/ac/su/tabletest/MainActivity.java
+++ app/src/main/java/th/ac/su/tabletest/MainActivity.java
@@ -1,8 +1,18 @@
 package th.ac.su.tabletest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dateText = findViewById(R.id.dateText);
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC+7"));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        dateText.setText(dtf.format(now));
+
+        Button summitBtn = findViewById(R.id.summit_btn);
+        summitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("Notification");
+                dialog.setMessage("Hello!");
+                dialog.setPositiveButton("OK", null);
+                dialog.show();
+            }
+        });
+
     }
 }
